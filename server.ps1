a749a8642c9eca1fc609aa9acb311b141e2d6022
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-06-03 20:19:42 -0500

    Update server.ps1
    resolved errors in logic

--- server.ps1
+++ server.ps1
@@ -1,6 +1,9 @@
-function Get-ContentType($filePath) {
-    foreach ($extension in $contentTypes.Keys) {
-        if ($filePath -match "\.$extension$") {
+function Get-ContentType($filePath)
+{    
+    foreach ($extension in $contentTypes.Keys)
+    {
+        if ($filePath -match "\.$extension$")
+        {
             return $contentTypes[$extension]
         }
     }
@@ -35,46 +38,53 @@ $listener.Prefixes.Add("http://*:$port/")
 $listener.Start()
 Write-Host "Listening at all IP addresses on port $port..."
 
-while ($true) {
+while ($true)
+{
     $context = $listener.GetContext()
     $request = $context.Request
     $response = $context.Response
 
-    try {
-        if ($request.HttpMethod -eq 'GET') {
-        $localPath = $request.Url.LocalPath
-        if ($localPath -eq '/') { $localPath = '/index.html' }
-
-        $filePath = [System.IO.Path]::GetFullPath("$webfiles$localPath").TrimStart('.')
-
-        Write-Host "Requested local path: $localPath"
-        Write-Host "Constructed file path: $filePath"
-            
-        if ($filePath.StartsWith($webfiles) -and (Test-Path $filePath)) {
-        $response.ContentType = Get-ContentType $filePath
-
-        $extension = [System.IO.Path]::GetExtension($filepath)
-
-        if ($extension -in $binaryExtensions) {
-                $buffer = [System.IO.File]::ReadAllBytes($filePath)
-            } else {
-                $content = Get-Content $filePath -Raw
+    try
+    {
+        if ($request.HttpMethod -eq 'GET')
+        {
+            $localPath = $request.Url.LocalPath
+            if ($localPath -eq '/')
+            {
+                $localPath = '/index.html'
+            }
+            $filePath = [System.IO.Path]::GetFullPath("$webfiles$localPath").TrimStart('.')
+            Write-Host "Requested local path: $localPath"
+            Write-Host "Constructed file path: $filePath"
+            if ($filePath.StartsWith($webfiles) -and (Test-Path $filePath))
+            {
+                $response.ContentType = Get-ContentType $filePath
+                $extension = [System.IO.Path]::GetExtension($filepath)
+                if ($extension -in $binaryExtensions)
+                {
+                    $buffer = [System.IO.File]::ReadAllBytes($filePath)
+                }
+                else
+                {
+                    $content = Get-Content $filePath -Raw
+                    $buffer = [System.Text.Encoding]::UTF8.GetBytes($content)
+                }
+            }
+            else
+            {
+                Write-Host "File not found: $filePath"
+                $response.StatusCode = 404
+                $content = 'File not found'
                 $buffer = [System.Text.Encoding]::UTF8.GetBytes($content)
             }
-        }
-    } else {
-        Write-Host "File not found: $filePath"
-        $response.StatusCode = 404
-        $content = 'File not found'
-        $buffer = [System.Text.Encoding]::UTF8.GetBytes($content)
-    }
-
             $response.ContentLength64 = $buffer.Length
             $output = $response.OutputStream
             $output.Write($buffer, 0, $buffer.Length)
             $output.Close()
-        } else {
-            $response.StatusCode = 405  # Method Not Allowed
+        }
+        else
+        {
+            $response.StatusCode = 405
             $content = 'Method not allowed'
             $buffer = [System.Text.Encoding]::UTF8.GetBytes($content)
             $response.ContentLength64 = $buffer.Length
@@ -83,7 +93,8 @@ while ($true) {
             $output.Close()
         }
     }
-    catch {
+    catch
+    {
         $response.StatusCode = 400
         $content = 'Bad Request'
         $buffer = [System.Text.Encoding]::UTF8.GetBytes($content)
